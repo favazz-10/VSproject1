e88fc70b288411e518c492da79cf084df095e010
favazz-10 <favaz2001@gmail.com>
2022-09-19 16:00:36 +0530

    commit 2

--- bin/dartvs.dart
+++ bin/dartvs.dart
@@ -1,5 +1,27 @@
-import 'package:dartvs/dartvs.dart' as dartvs;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dartvs.calculate()}!');
+void main() {
+  String name = 'favaz';
+  int age = 21;
+  double mark = 8.5;
+  String email = "favaz2001@gmail.com";
+  int ph = 8078081609;
+  int a = 10;
+  int b = 50;
+
+  String s = 'sum';
+  String d = 'difference';
+  String m = "multiplication";
+  String D = 'division';
+
+  print("my name is $name");
+  print('iam $age years old');
+  print('my email id is $email');
+  print('my mark is $mark');
+  print("$ph is my ph number");
+  stdout.write('this is my first dart project');
+  print('\n$s=${a + b}');
+  print('$d=${a - b}');
+  print('$m=${a * b}');
+  print('$D=${a / b}');
 }
